98e38003befc1f535b1856c5872e640b901753f5
Adam Bures <adam.bures.prg@gmail.com>
2024-07-06 22:43:42 +0200

    create battle with rockets

--- battle.mjs
+++ battle.mjs
@@ -1,3 +1,8 @@
+import { GertrudaAI } from './gertrudaAI.mjs'
+import { Army } from './army.mjs';
+import { Ship } from './ship.mjs';
+import { Dice } from './dice.mjs';
+import { Component } from './component.mjs';
 
 class Battle {
     constructor(army0, army1) {
@@ -14,6 +19,21 @@ class Battle {
     createBattleOrder() {
         const allSorted = this.all.sort((a, b) => {return b.getAgility() - a.getAgility()});
         return allSorted;
+    }
+
+    rocketBattle(ai) {
+        this.createRocketsBattleOrder().forEach(ship => {
+            if(ship.isExploded) {return}
+            let rockets = ship.getRockets();
+            rockets.forEach(r => {
+                let ro = r.dice.roll();
+                if(ro > 1) {
+                    let hitting = ai.selectShipToHit(ship.army === this.army1 ? this.army0 : this.army1, ship, r, ro);
+                    console.log('We are aiming for: ' + hitting);
+                    hitting.recieveDamage(r.damage);
+                }
+            })
+        });
     }
 }
 
